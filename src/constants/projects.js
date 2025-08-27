bf8fa3b7b74695fe65f9a4ce6b32d82fdf53365c
Dgomisprz <dgomiperez@gmail.com>
2025-08-27 13:20:22 +0200

    added project

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -3,7 +3,7 @@ const projects = [
     id: 1,
     title: "WatchOver",
     description: "A anime catalog web app where you can find your favorite animes, search information, stay up to date with the latest episodes, make your own list and more.",
-    image: "assets/watchover.png",
+    image: "assets/watchover.webp",
     link: "https://github.com/Dgomisprz/watchover-laravel",
     technologies: ["laravel", "bootstrap", "javascript", "html5", "css3"],
   },
@@ -11,9 +11,17 @@ const projects = [
     id: 2,
     title: "NakamaDex (Work in progress)",
     description: "A One Piece TCG app where you can organize and track your collection, build competitive decks, stay up to date with game updates, and enjoy a simple, visual, and intuitive platform. ",
-    image: "assets/Portada.png",
+    image: "assets/Portada.webp",
     link: "",
     technologies: ["flutter", "visualstudiocode"],
+  },
+  {
+    id: 3,
+    title: "Email Assistant",
+    description: "An AI-powered email assistant app where you can draft and refine messages, boost productivity with smart suggestions, and enjoy a simple, intuitive, and personal platform. Developed for personal use.",
+    image: "assets/correoapp.webp",
+    link: "",
+    technologies: ["flutter", "visualstudiocode", "openai" ],
   },
   // Agrega más proyectos según necesites
 ];
